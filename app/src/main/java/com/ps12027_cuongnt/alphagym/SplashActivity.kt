70810359da2ddac189c86c9c17e6200217845055
CG2581 <cg2581.dev@gmail.com>
2020-09-19 00:35:41 +0700

    Initial commit

--- app/src/main/java/com/ps12027_cuongnt/alphagym/SplashActivity.kt
+++ app/src/main/java/com/ps12027_cuongnt/alphagym/SplashActivity.kt
@@ -1,23 +1,40 @@
 package com.ps12027_cuongnt.alphagym
 
 import android.content.Intent
-
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 
+
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+    var clLogo: ConstraintLayout? = null
+    var splashProgress: ProgressBar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        splashProgress = findViewById(R.id.splashProgress)
+        clLogo = findViewById(R.id.clLogo)
+        val animation =
+            AnimationUtils.loadAnimation(this, R.anim.logoscroll)
+        clLogo?.startAnimation(animation)
 
+        val time: Long = 5000
         Handler().postDelayed(Runnable {
-            val mySuperIntent = Intent(this, MainActivity::class.java)
-            startActivity(mySuperIntent)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
             finish()
-        }, 3000)
+        }, time)
+
+
     }
 }
 
